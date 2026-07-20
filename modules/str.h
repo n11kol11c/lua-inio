49cd9fa76320f5a531876f7889690638a49d64df
м <174154109+n11kol11c@users.noreply.github.com>
2026-07-20 22:09:10 +0200

    Add string utility functions in str.h

---
+++ modules/str.h
@@ -0,0 +1,9 @@
+#ifndef STR_H
+#define STR_H
+
+int startswith(const char *str, char ch);
+int endswith(const char *str, char ch);
+char *trim(char *str);
+int contains(const char *str, const char *target);
+
+#endif
